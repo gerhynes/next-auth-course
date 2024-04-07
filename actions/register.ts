f3c6f43e856373411153c397ef1097c4d9561b8a
gerhynes <gkhynes@gmail.com>
2024-04-07 11:54:55 +0100

    Add registration functionality

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export async function register(values: z.infer<typeof RegisterSchema>) {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +13,25 @@ export async function register(values: z.infer<typeof RegisterSchema>) {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Email sent" };
+  const { name, email, password } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TOOD Send verification token email
+
+  return { success: "User created" };
 }
